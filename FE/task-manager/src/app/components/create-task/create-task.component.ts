c07cf7fc03f8688eb1408eba2b4607313938d7ad
AndreyGoranov <agoranovreff@gmail.com>
2020-09-15 21:27:27 +0300

    added focus on title input field -added template referance variable -added ViewChild decorator -added AfterViewInit hook -using the Renderer2 class to manipulate element without touching the DOM directly

--- FE/task-manager/src/app/components/create-task/create-task.component.ts
+++ FE/task-manager/src/app/components/create-task/create-task.component.ts
@@ -1,6 +1,6 @@
 import { Router } from '@angular/router';
 import { TaskManipulationService } from './../../services/tasks-crud-operations/task-manipulation.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Renderer2 } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Task } from 'src/app/interfaces/task-interface';
 
@@ -10,18 +10,27 @@ import { Task } from 'src/app/interfaces/task-interface';
   templateUrl: './create-task.component.html',
   styleUrls: ['./create-task.component.css']
 })
-export class CreateTaskComponent implements OnInit {
+export class CreateTaskComponent implements OnInit,  AfterViewInit {
 
-  constructor(private fb: FormBuilder, private taskService: TaskManipulationService, private router: Router) { }
+  constructor(private fb: FormBuilder,
+              private taskService: TaskManipulationService,
+              private router: Router,
+              private renderer: Renderer2) { }
 
   createTask: FormGroup;
   tasks: Task[];
+  @ViewChild('titleInputRef')
+  titleInputRef: ElementRef;
 
   ngOnInit(): void {
     this.createTask = this.fb.group({
       title: ['', Validators.required],
       description: ['']
     });
+  }
+
+  ngAfterViewInit(): void {
+    this.renderer.selectRootElement(this.titleInputRef.nativeElement).focus();
   }
 
   saveTask(): any {
